7f4d17afbaeeb725a46de15aa58fd6ce2ac6b1d2
SpeedDevelopment <austrieshane@gmail.com>
2012-04-29 11:23:20 -0300

    Finishing Touches (2/3)

--- products/triumph.mk
+++ products/triumph.mk
@@ -14,8 +14,8 @@ PRODUCT_MODEL := Triumph
 PRODUCT_MANUFACTURER := Motorola
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=triumph BUILD_ID=GRJ22 BUILD_DISPLAY_ID=GWK74 BUILD_FINGERPRINT=Motorola/Caymus_VRGUS/Triumph/Triumph:2.2.2/FROYO/1010_2_039_110607:user/release-keys PRIVATE_BUILD_DESC="Caymus_VRGUS-user 2.2.2 FROYO 1010_2_039 release-keys"
 
-# Extra Triumph overlay
-PRODUCT_PACKAGE_OVERLAYS += vendor/aokp/overlay/triumph
+# Release name and versioning
+PRODUCT_RELEASE_NAME := Triumph
 
 #
 # Copy passion specific prebuilt files
