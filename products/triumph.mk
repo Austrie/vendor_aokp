2fb0ffa7098e5325c7d1cf32cfbbd494e5a2b800
SpeedDevelopment <austrieshane@gmail.com>
2012-04-29 13:27:12 -0300

    Fixed...

--- products/triumph.mk
+++ products/triumph.mk
@@ -12,7 +12,7 @@ PRODUCT_BRAND := Motorola
 PRODUCT_DEVICE := triumph
 PRODUCT_MODEL := Triumph
 PRODUCT_MANUFACTURER := Motorola
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=triumph BUILD_ID=GRJ22 BUILD_DISPLAY_ID=GWK74 BUILD_FINGERPRINT=Motorola/Caymus_VRGUS/Triumph/Triumph:2.2.2/FROYO/1010_2_039_110607:user/release-keys PRIVATE_BUILD_DESC="Caymus_VRGUS-user 2.2.2 FROYO 1010_2_039 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES := PRODUCT_NAME=triumph BUILD_ID=GRJ22 BUILD_DISPLAY_ID=GWK74 BUILD_FINGERPRINT=Motorola/Caymus_VRGUS/Triumph/Triumph:2.2.2/FROYO/1010_2_039_110607:user/release-keys PRIVATE_BUILD_DESC="Caymus_VRGUS-user 2.2.2 FROYO 1010_2_039 release-keys"
 
 # Release name and versioning
 PRODUCT_RELEASE_NAME := Triumph
@@ -21,14 +21,4 @@ PRODUCT_RELEASE_NAME := Triumph
 # Copy passion specific prebuilt files
 #
 PRODUCT_COPY_FILES +=  \
-    vendor/aokp/prebuilt/common/app/UnicornPorn.apk:system/app/UnicornPorn.apk \
-    vendor/aokp/prebuilt/common/app/Microbes.apk:system/app/Microbes.apk \
-    vendor/aokp/prebuilt/common/app/Superuser.apk:system/app/Superuser.apk \
-    vendor/aokp/prebuilt/common/app/LatinImeDictionaryPack.apk:system/app/LatinImeDictionaryPack.apk \
-    vendor/aokp/prebuilt/common/lib/libmicrobes_jni.so:system/app/libmicrobes_jni.so \
-    vendor/aokp/prebuilt/common/etc/apns-conf.xml:system/etc/apns-confs.xml \
-    vendor/aokp/prebuilt/common/etc/init.local.rc:system/etc/init.local.rc \
-    vendor/aokp/prebuilt/common/etc/resolv.conf:system/etc/resolv.conf \
-    vendor/aokp/prebuilt/xhdpi/bootanimation.zip:system/media/bootanimation.zip \
-    vendor/aokp/prebuilt/common/etc/init.d/_placeholder_:system/etc/init.d/_placeholder_ \
-    vendor/aokp/prebuilt/tuna/vold.fstab:system/etc/vold.fstab
+    vendor/aokp/prebuilt/common/media/bootanimation.zip:system/media/bootanimation.zip
